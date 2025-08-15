63bdf27b817830b81e0678d47eb49674584b11f1
Andy Zhang <zhangnd16@gmail.com>
2025-08-14 21:16:18 -0700

    corpus change to aflplusplus/run.sh

--- fuzzers/aflplusplus/run.sh
+++ fuzzers/aflplusplus/run.sh
@@ -25,6 +25,6 @@ export AFL_NO_UI=1
 export AFL_MAP_SIZE=256000
 export AFL_DRIVER_DONT_DEFER=1
 
-"$FUZZER/repo/afl-fuzz" -i "$TARGET/corpus/$PROGRAM" -o "$SHARED/findings" \
+"$FUZZER/repo/afl-fuzz" -i "$TARGET/$CORPUS/$PROGRAM" -o "$SHARED/findings" \
     "${flag_cmplog[@]}" -d \
     $FUZZARGS -- "$OUT/afl/$PROGRAM" $ARGS 2>&1
